138e06f7b3fcfb8eafe3e47d842a7f562ee923b8
EgorPetrov1995 <32413259+EgorPetrov1995@users.noreply.github.com>
2017-09-30 19:18:24 +0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,32 +1,32 @@
 /**************************************************************************************************************/
 
-/***********************************    Функции для игры "Убей бомжа"    **************************************/
+/***********************************    Г”ГіГ­ГЄГ¶ГЁГЁ Г¤Г«Гї ГЁГЈГ°Г» "Г“ГЎГҐГ© ГЎГ®Г¬Г¦Г "    **************************************/
 
 /* 30.09.2017 */
 /**************************************************************************************************************/
-var ammo_start; // Стартовое количество боеприпасов
+var ammo_start; // Г‘ГІГ Г°ГІГ®ГўГ®ГҐ ГЄГ®Г«ГЁГ·ГҐГ±ГІГўГ® ГЎГ®ГҐГЇГ°ГЁГЇГ Г±Г®Гў
 var killCount;
 var isStart = false;
 
-function getRand(min, max)  // Генерация случайного числа
+function getRand(min, max)  // ГѓГҐГ­ГҐГ°Г Г¶ГЁГї Г±Г«ГіГ·Г Г©Г­Г®ГЈГ® Г·ГЁГ±Г«Г 
 {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
   
 
-var table = document.getElementById('bitches');  // Функция отслеживания нажатия
+var table = document.getElementById('bitches');  // Г”ГіГ­ГЄГ¶ГЁГї Г®ГІГ±Г«ГҐГ¦ГЁГўГ Г­ГЁГї Г­Г Г¦Г ГІГЁГї
 	
 	document.getElementById('bitches').onclick = function(e) 
-	{ // Вешаем обработчик
+	{ // Г‚ГҐГёГ ГҐГ¬ Г®ГЎГ°Г ГЎГ®ГІГ·ГЁГЄ
 
-	var event = e || window.event, // Получаем event.target 
+	var event = e || window.event, // ГЏГ®Г«ГіГ·Г ГҐГ¬ event.target 
       target = event.CurrentTarget || event.srcElement; 
 	if( isStart == true)
 	{
 	if (target.tagName == 'IMG' )
-	{ //Проверяем, попали в бомжа или нет
+	{ //ГЏГ°Г®ГўГҐГ°ГїГҐГ¬, ГЇГ®ГЇГ Г«ГЁ Гў ГЎГ®Г¬Г¦Г  ГЁГ«ГЁ Г­ГҐГІ
 		{
-		killCount++; // Убили бомжа
+		killCount++; // Г“ГЎГЁГ«ГЁ ГЎГ®Г¬Г¦Г 
 		Shot();  
 		PlaySnd('sounds/death.wav'); 
 		target.style.backgroundImage = 'url(images/none.png)';
@@ -44,39 +44,39 @@ var table = document.getElementById('bitches');  // Функция отслеживания нажатия
  {
 	 if (isStart == true)
 	 {
-			PlaySnd('sounds/shot.mp3'); // Воспроизводим звук выстрела
-			ammo_start--; // Убираем патрон
-			infoUpdate(); // Обновляем информацию
+			PlaySnd('sounds/shot.mp3'); // Г‚Г®Г±ГЇГ°Г®ГЁГ§ГўГ®Г¤ГЁГ¬ Г§ГўГіГЄ ГўГ»Г±ГІГ°ГҐГ«Г 
+			ammo_start--; // Г“ГЎГЁГ°Г ГҐГ¬ ГЇГ ГІГ°Г®Г­
+			infoUpdate(); // ГЋГЎГ­Г®ГўГ«ГїГҐГ¬ ГЁГ­ГґГ®Г°Г¬Г Г¶ГЁГѕ
 	 }
 	 else
 	 {
-		 alert("Начните игру!");
+		 alert("No ammo!");
 	 }
  }
  
- function PlaySnd(path)  // Проигрывание звука (path) - путь к файлу
+ function PlaySnd(path)  // ГЏГ°Г®ГЁГЈГ°Г»ГўГ Г­ГЁГҐ Г§ГўГіГЄГ  (path) - ГЇГіГІГј ГЄ ГґГ Г©Г«Гі
  {
 	 var snd = new Audio();
 	 snd.src = path;
 	 snd.autoplay = true;
  }
  
- // Старт игры
+ // Г‘ГІГ Г°ГІ ГЁГЈГ°Г»
  function Start()
  {
-	 var S=prompt("Привет, как вас зовут?","");
-	 isStart = true; //Игра запущена
-	 PlaySnd('sounds/reload.mp3'); // Заряжаем оружие
-	 ammo_start = 40; // Даем стартовый боезапас
-	 killCount = 0; // Обнуляем счётчик убитых бомжей
-	 infoUpdate() // Обновляем информацию
-	 CreateBums(); // Создаем бомжей
+	 var S=prompt("What is your name?","");
+	 isStart = true; //Г€ГЈГ°Г  Г§Г ГЇГіГ№ГҐГ­Г 
+	 PlaySnd('sounds/reload.mp3'); // Г‡Г Г°ГїГ¦Г ГҐГ¬ Г®Г°ГіГ¦ГЁГҐ
+	 ammo_start = 40; // Г„Г ГҐГ¬ Г±ГІГ Г°ГІГ®ГўГ»Г© ГЎГ®ГҐГ§Г ГЇГ Г±
+	 killCount = 0; // ГЋГЎГ­ГіГ«ГїГҐГ¬ Г±Г·ВёГІГ·ГЁГЄ ГіГЎГЁГІГ»Гµ ГЎГ®Г¬Г¦ГҐГ©
+	 infoUpdate() // ГЋГЎГ­Г®ГўГ«ГїГҐГ¬ ГЁГ­ГґГ®Г°Г¬Г Г¶ГЁГѕ
+	 CreateBums(); // Г‘Г®Г§Г¤Г ГҐГ¬ ГЎГ®Г¬Г¦ГҐГ©
  }
  
  function infoUpdate()
  {
-	document.getElementById('killCount').innerHTML = 'Убито бомжей:' + killCount; 
-	document.getElementById('ammo_count').innerHTML = 'Боеприпасы:' + ammo_start;
+	document.getElementById('killCount').innerHTML = 'Ammo:' + killCount; 
+	document.getElementById('ammo_count').innerHTML = 'Kills:' + ammo_start;
     if(ammo_start == 0)
 	{
 		PlaySnd('sounds/no_ammo.wav');
